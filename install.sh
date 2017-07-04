b9d1c2c304d5f30affbb6f656b5c0eebedfd92b9
Jonathan Merritt <j.s.merritt@gmail.com>
2017-07-04 11:55:35 +1000

    Initial checkin - stuff from other repos and setup

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+#
+# # Install / update script for dotfiles
+#
+# This script installs the user configuration. It should be idempotent (ie. it
+# can be run multiple times without changing the result), and thus functions as
+# both an installer and updater.
+
+# "Safe" bash
+set -euf -o pipefail
+
+# Base directory of this script
+declare -r base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Directories within the repo
+declare -r home_dir="${base_dir}/home"
+declare -r nix_dir="${base_dir}/nix"
+
+# Log function
+# Usage: log 'Message'
+function log() {
+    local msg="$1"
+    echo "* install.sh: ${msg}"
+}
+
+# Link dotfiles
+dotfiles=( '.spacemacs' \
+           '.zshrc'     \
+         )
+function dotfile_ln() {
+    local src="${home_dir}/$1"
+    local tgt="${HOME}/$1"
+    log "Linking: ${tgt} -> ${src}"
+    ln -sf "${src}" "${tgt}"
+}
+for dotfile in "${dotfiles[@]}"; do dotfile_ln "${dotfile}"; done
+
+# Install nix if necessary
+if [ ! -d '/nix' ]; then
+    log "/nix directory was not found: installing nix"
+    bash <(curl https://nixos.org/nix/install)
+else
+    log "/nix directory exists; not re-installing nix"
+fi
+
+# Copy the nix config across
+mkdir -p "${HOME}/.nixpkgs"
+function nixconfig_ln() {
+    local src="${nix_dir}/config.nix"
+    local tgt="${HOME}/.nixpkgs/config.nix"
+    log "Linking: ${tgt} -> ${src}"
+    ln -sf "${src}" "${tgt}"
+}
+nixconfig_ln
---
+++ nix/config.nix
@@ -0,0 +1,53 @@
+{
+  packageOverrides = pkgs: rec {
+
+    cntlm = with pkgs.stdenv.lib;
+      let
+        version = "0.92.3";
+        name = "cntlm-${version}";
+      in pkgs.stdenv.mkDerivation {
+        inherit name;
+
+        src = pkgs.fetchurl {
+          url = "https://downloads.sourceforge.net/project/cntlm/cntlm/cntlm%20${version}/cntlm-${version}.tar.bz2";
+          sha256 = "7b603d6200ab0b26034e9e200fab949cc0a8e5fdd4df2c80b8fc5b1c37e7b930";
+        };
+
+        buildInputs = with pkgs; [
+          which
+        ];
+
+        preConfigure = ''
+          sed -e 's/gcc/clang/' Makefile > Makefile.clang
+          sed -i 's/CSS=.*/CSS="xlc_r gcc clang"/' configure
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin; cp cntlm $out/bin/;
+          mkdir -p $out/share/; cp COPYRIGHT README VERSION doc/cntlm.conf $out/share/;
+          mkdir -p $out/man/; cp doc/cntlm.1 $out/man/;
+        '';
+      };
+
+    coreEnv = with pkgs; buildEnv {
+      name = "coreEnv";
+      paths = [
+        ag
+        cmake
+        cntlm
+        curl
+        emacs25Macport
+        git
+        html-tidy
+        neovim
+        nodePackages.tern
+        stack
+        travis
+        wget
+        which
+        zsh
+      ];
+    };
+
+  };
+}
