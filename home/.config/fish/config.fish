67c292f99983448b63e108ed8c603b597c5f3732
Jonathan Merritt <j.s.merritt@gmail.com>
2018-03-07 09:39:36 +1100

    Add gsbt-proxy function

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -91,3 +91,13 @@ function gsbt
   set GJAVA_OPTS ''
   eval "env JAVA_OPTS='$GJAVA_OPTS' SBT_OPTS='$GSBT_OPTS' sbt $GCMD_OPTS $argv"
 end
+
+# SBT using global repository settings and proxy
+function gsbt-proxy
+  set GCMD_OPTS '-J-Xmx4g -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC -J-Xss2M'
+  set PROXY_OPTS '-J-Dhttp.proxyHost=127.0.0.1 -J-Dhttp.proxyPort=3128 -J-Dhttps.proxyHost=127.0.0.1 -J-Dhttps.proxyPort=3128'
+  set GSBT_OPTS ''
+  set GJAVA_OPTS ''
+  eval "env JAVA_OPTS='$GJAVA_OPTS' SBT_OPTS='$GSBT_OPTS' sbt $GCMD_OPTS $PROXY_OPTS $argv"
+end
+
