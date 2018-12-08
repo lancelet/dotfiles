c44c21c5c7d88f63eff82ca90ab6409f8b6469a0
Jonathan Merritt <j.s.merritt@gmail.com>
2018-12-08 14:39:05 +1100

    Haskell tweaks

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -11,14 +11,14 @@
 
 (load-theme 'doom-nord-light t)
 
-(add-hook! haskell-mode
-  ;; before expressions in a Haskell layout list
-  (setq-default haskell-indentation-layout-offset 4)
-  ;; after an opening keyword (eg let)
-  (setq-default haskell-indentation-starter-offset 4)
-  ;; after an indentation to the left (eg do)
-  (setq-default haskell-indentation-left-offset 4)
-  ;; before the keyword where
-  (setq-default haskell-indentation-where-pre-offset 2)
-  ;; after the keyword where
-  (setq-default haskell-indentation-where-post-offset 2))
+;; (add-hook! haskell-mode
+;;   ;; before expressions in a Haskell layout list
+;;   (setq-default haskell-indentation-layout-offset 4)
+;;   ;; after an opening keyword (eg let)
+;;   (setq-default haskell-indentation-starter-offset 4)
+;;   ;; after an indentation to the left (eg do)
+;;   (setq-default haskell-indentation-left-offset 4)
+;;   ;; before the keyword where
+;;   (setq-default haskell-indentation-where-pre-offset 2)
+;;   ;; after the keyword where
+;;   (setq-default haskell-indentation-where-post-offset 2))
