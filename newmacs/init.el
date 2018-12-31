1193be89a6cd1a43a1adb47117ee00a15b9a07d5
Jonathan Merritt <j.s.merritt@gmail.com>
2018-12-31 16:57:05 +1100

    Tweaking newmacs config again

--- newmacs/init.el
+++ newmacs/init.el
@@ -1,28 +1,55 @@
-;; This loads an Org mode file and builds the configuration code out of it.
+;;; init.el --- GNU Emacs Configuration
 
-;; The path of a directory with operates like .emacs.d
-(defconst dotemacs-path (file-name-directory load-file-name)
-  "Location of the equivalent of the .emacs.d directory.")
-(message "dotemacs-path is `%s'" dotemacs-path)
+;;; Basics inspired from: https://github.com/rememberYou/.emacs.d
 
-(let ((gc-cons-threshold most-positive-fixnum))
+;; This program is free software. You can redistribute it and/or modify it under
+;; the terms of the Do What The Fuck You Want To Public License, version 2 as
+;; published by Sam Hocevar.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.
+;;
+;; You should have received a copy of the Do What The Fuck You Want To Public
+;; License along with this program. If not, see http://www.wtfpl.net/.
 
-  ;; Set repositories that we'll pull packages from
-  (require 'package)
-  (setq-default
-   load-prefer-newer t
-   package-enable-at-startup nil)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-  (package-initialize)
+;;; Code:
 
-  ;; Install dependencies
-  (unless (and (package-installed-p 'delight)
-               (package-installed-p 'use-package))
-    (package-refresh-contents)
-    (package-install 'delight t)
-    (package-install 'use-package t))
+;; Set directories.
+(setq user-init-file load-file-name)
+(setq user-emacs-directory (file-name-directory user-init-file))
 
-  ;; Use an org file for remainder of configuration
-  (org-babel-load-file (expand-file-name "dotemacs.org" dotemacs-path))
+;; Make startup faster by reducing the frequency of garbage
+;; collection.
+(setq gc-cons-threshold (* 50 1000 1000))
 
-  (garbage-collect))
+(require 'package)
+(package-initialize)
+
+;; Load org mode config
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
+
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-begin-commands (quote (self-insert-command)) t)
+ '(company-idle-delay 0.1 t)
+ '(company-minimum-prefix-length 2 t)
+ '(company-show-numbers t t)
+ '(company-tooltip-align-annotations t t)
+ '(global-company-mode t t)
+ '(package-selected-packages
+   (quote
+    (general evil-leader evil-goggles doom-themes use-package-ensure-system-package evil delight))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
