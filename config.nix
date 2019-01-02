d5a7e6cd71fe4136ae4618ea0f70e96292eaa052
Jonathan Merritt <j.s.merritt@gmail.com>
2019-01-02 16:14:09 +1100

    Initial version that installs Nix

---
+++ config.nix
@@ -0,0 +1,98 @@
+{
+  packageOverrides = pkgs: rec {
+
+    cntlm = with pkgs.stdenv.lib;
+      let
+        version = "0.92.3";
+        name = "cntlm-${version}";
+      in pkgs.stdenv.mkDerivation {
+        inherit name;
+
+        src = pkgs.fetchurl {
+          url = "https://downloads.sourceforge.net/project/cntlm/cntlm/cntlm%20${version}/cntlm-${version}.tar.bz2";
+          sha256 = "7b603d6200ab0b26034e9e200fab949cc0a8e5fdd4df2c80b8fc5b1c37e7b930";
+        };
+
+        buildInputs = with pkgs; [
+          which
+        ];
+
+        preConfigure = ''
+          sed -e 's/gcc/clang/' Makefile > Makefile.clang
+          sed -i 's/CSS=.*/CSS="xlc_r gcc clang"/' configure
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin; cp cntlm $out/bin/;
+          mkdir -p $out/share/; cp COPYRIGHT README VERSION doc/cntlm.conf $out/share/;
+          mkdir -p $out/man/; cp doc/cntlm.1 $out/man/;
+        '';
+      };
+
+    coreEnv = with pkgs; buildEnv {
+      name = "coreEnv";
+      paths = [
+        ag
+        ansible
+        aspell
+        aspellDicts.en
+        autoconf
+        automake
+        cabal-install
+        cabal2nix
+        cmake
+        cntlm
+        curl
+        emacs26
+        enca
+        ffmpeg
+        fish
+        fswatch
+        ghc
+        git
+        gnupg
+        haskellPackages.alex
+        haskellPackages.fast-tags
+        haskellPackages.ghcid
+        haskellPackages.happy
+        haskellPackages.hasktags
+        haskellPackages.hlint
+        haskellPackages.hoogle
+        haskellPackages.pandoc
+        haskellPackages.stylish-haskell
+        html-tidy
+        idris
+        jq
+        leiningen
+        libffi
+        llvm_5
+        maven
+        ncurses
+        ncurses.dev
+        neovim
+        nix-prefetch-git
+        nodePackages.tern
+        openssl
+        openssl.dev  # for Laurence's confluence sync tool
+        pkgconfig
+        python36
+        python36Packages.ipython
+        python36Packages.pip
+        python36Packages.virtualenv
+        rustup
+        scala
+        shellcheck
+        stack
+        texlive.combined.scheme-full
+        travis
+        tree
+        vagrant
+        vault
+        wget
+        which
+        youtube-dl
+      ];
+    };
+
+  };
+}
--- update.hs
+++ update.hs
@@ -2,8 +2,128 @@
 {- cabal:
 build-depends: 
     base   ^>= 4.12
-  , shelly ^>= 1.8
+  , text   ^>= 1.2
+  , turtle ^>= 1.5
 -}
 
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad (unless, when)
+import Turtle (FilePath, Shell, sh, empty, inshell, isDirectory, stat, directory, mktree, home, (</>), toText, pwd,
+               symlink, isSymbolicLink, rm, testfile, liftIO, rmtree, testdir, testpath, (.&&.), (.||.), shell,
+               ExitCode)
+import Data.Text (Text)
+import qualified Data.Text as T
+
+import Prelude hiding (FilePath)
+
 main :: IO ()
-main = putStrLn "Hello world!"
+main = do
+    putStrLn "Hello world!"
+    sh everything
+
+everything :: Shell ()
+everything = do
+    linkDotFiles
+    nix
+
+-- | Force a symbolic link, removing an existing file if present.
+forceLink :: FilePath -> FilePath -> Shell ()
+forceLink source dest = do
+    -- if the file exists then remove it
+    exists <- testfile dest
+    when exists (rm dest)
+    -- set up the new symbolic link
+    symlink source dest 
+
+-- | Force removal of a file.
+rmtreef :: FilePath -> Shell ()
+rmtreef path = do
+    exists <- testpath path
+    when exists (rmtree path)
+
+-----
+-- Actual dotfiles
+-----
+
+data DotFile
+  = DotFile
+    { inRepo :: FilePath
+    , inDir  :: FilePath
+    }
+
+dotFiles :: [DotFile]
+dotFiles
+  = [ DotFile "config.nix" ".nixpkgs/config.nix"
+    ]
+
+-- | Link dotfiles.
+linkDotFiles :: Shell ()
+linkDotFiles = sequence_ $ linkDotFile <$> dotFiles
+
+-- | Link a single dotfile.
+linkDotFile :: DotFile -> Shell ()
+linkDotFile dotFile = do
+    curDir <- pwd
+    homeDir <- home
+    let 
+        sourceFile = curDir </> (inRepo dotFile)
+        targetDir = homeDir </> (directory . inDir $ dotFile)
+        targetFile = homeDir </> (inDir dotFile)
+    mktree targetDir 
+    forceLink sourceFile targetFile
+    pure ()
+
+-- | Unlink any installed dotfiles.
+unLinkDotFiles :: Shell ()
+unLinkDotFiles = sequence_ $ unlinkDotFile <$> dotFiles
+
+-- | Unlink a single dotfile.
+unlinkDotFile :: DotFile -> Shell ()
+unlinkDotFile dotFile = do
+    homeDir <- home
+    let
+        targetFile = homeDir </> (inDir dotFile)
+    exists <- testfile targetFile
+    when exists (rm targetFile)
+
+-----
+-- Nix
+-----
+
+-- | Check if Nix is installed already.
+nixInstalled :: Shell Bool
+nixInstalled = testdir "/nix"
+
+inNix :: Text -> Shell ExitCode
+inNix cmd = do 
+    let fullCmd = T.concat [ "source ~/.nix-profile/etc/profile.d/nix.sh && ", cmd ]
+    shell fullCmd empty
+
+-- | Nix cycle.
+nix :: Shell ()
+nix = do
+    installed <- nixInstalled
+    unless installed $ do
+        -- The nix installer will complain if the dotfiles reference Nix,
+        -- so we have to un-link them temporarily.
+        unLinkDotFiles
+        homeDir <- home
+        rmtreef (homeDir </> ".nix-channels")
+        rmtreef (homeDir </> ".nix-defexpr")
+        rmtreef (homeDir </> ".nix-profile")
+        rmtreef (homeDir </> ".nixpkgs")
+        installNix
+        linkDotFiles
+    inNix "nix-channel --remove nixpkgs"
+    inNix "nix-channel --add https://nixos.org/channels/nixpkgs-18.09-darwin nixpkgs"
+    inNix "nix-channel --update"
+    inNix "nix-env -iA nixpkgs.coreEnv"
+    pure ()
+
+-- | Install Nix.
+installNix :: Shell ()
+installNix = do
+    inshell "sh" (inshell "curl https://nixos.org/nix/install" empty)
+    pure ()
+
