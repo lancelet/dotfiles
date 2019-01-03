5734ed2b4a43be65dba1190e05755d20a57fd6a8
Jonathan Merritt <j.s.merritt@gmail.com>
2019-01-03 11:30:30 +1100

    Shellcheck fixes and documentation

--- install.sh
+++ install.sh
@@ -1,20 +1,54 @@
 #!/usr/bin/env bash
 #
 # Bootstrapping script for my dotfiles install.
+#
 # The purpose of this script is to hand off to Haskell.
 # Importantly, this script shouldn't touch the $HOME directory.
 
-# Constants.
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-readonly GHC_VERSION='8.6.3'
-readonly TOOL_DIR="${SCRIPT_DIR}/.tools"
-readonly GHCUP_SCRIPT="${TOOL_DIR}/ghcup"
-readonly GHCUP_INSTALL_BASE_PREFIX="${TOOL_DIR}"
-readonly CABAL_DIR="${TOOL_DIR}/.cabal"
-readonly CABAL_CONFIG="${TOOL_DIR}/cabal-config"
+####################
+# Global Constants #
+####################
 
-export CABAL_CONFIG
+# @VARIABLE: SCRIPT_DIR
+# @DESCRIPTION:
+# Directory of this script
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# @VARIABLE: GHC_VERSION
+# @DESCRIPTION:
+# Version of the Glasgow Haskell Compiler to use for bootstrapping.
+readonly GHC_VERSION='8.6.3'
+
+# @VARIABLE: TOOL_DIR
+# @DESCRIPTION:
+# Location to install tools required for this script.
+readonly TOOL_DIR="${SCRIPT_DIR}/.tools"
+
+# @VARIABLE: GHCUP_SCRIPT
+# @DESCRIPTION:
+# Location of the ghcup script (responsible for downloading GHC).
+readonly GHCUP_SCRIPT="${TOOL_DIR}/ghcup"
+
+# @VARIABLE: GHCUP_INSTALL_BASE_PREFIX
+# @DESCRIPTION:
+# Used by the ghcup script to locate its base install location.
+readonly GHCUP_INSTALL_BASE_PREFIX="${TOOL_DIR}"
 export GHCUP_INSTALL_BASE_PREFIX
+
+# @VARIABLE: CABAL_DIR
+# @DESCRIPTION:
+# Location of the .cabal directory used exclusively by this script.
+readonly CABAL_DIR="${TOOL_DIR}/.cabal"
+
+# @VARIABLE: CABAL_CONFIG
+# @DESCRIPTION:
+# Used by cabal to locate its configuration file.
+readonly CABAL_CONFIG="${TOOL_DIR}/cabal-config"
+export CABAL_CONFIG
+
+#####################
+# Script Operations #
+#####################
 
 # Create a directory for tooling.
 mkdir -p "${TOOL_DIR}"
@@ -23,15 +57,15 @@ mkdir -p "${TOOL_DIR}"
 if [ ! -f "${TOOL_DIR}/bin/xz" ]; then
     echo "${TOOL_DIR}/bin/xz not found: downloading and building..."
     curl -L https://tukaani.org/xz/xz-5.2.4.tar.bz2 > "${TOOL_DIR}/xz.tar.bz2"
-    pushd "${TOOL_DIR}" > /dev/null
+    pushd "${TOOL_DIR}" > /dev/null || exit
         mkdir xz
         tar -C xz --strip-components=1 -xf xz.tar.bz2
         rm xz.tar.bz2
-        cd xz
+        cd xz || exit
         ./configure --prefix="${TOOL_DIR}"
         make -j 5
         make install
-    popd
+    popd || exit
     rm -rf xz
 fi
 
@@ -52,7 +86,8 @@ if [ ! -f "${TOOL_DIR}/.ghcup/bin/ghc" ]; then
 fi
 
 # Write the Cabal config file.
-/bin/cat <<EOF >${CABAL_CONFIG}
+# This prevents Cabal from using the ${HOME}/.cabal file.
+/bin/cat <<EOF >"${CABAL_CONFIG}"
 repository hackage.haskell.org
   url: http://hackage.haskell.org/
 remote-repo-cache: ${CABAL_DIR}/packages
