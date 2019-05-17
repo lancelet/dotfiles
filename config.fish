22bd06099ce49b822595ed3459a2a20773652662
Jonathan Merritt <j.s.merritt@gmail.com>
2019-05-17 16:21:18 +1000

    Add additional sbt setup

--- config.fish
+++ config.fish
@@ -134,6 +134,23 @@ function gsbt-proxy
   eval "env JAVA_OPTS='$GJAVA_OPTS' SBT_OPTS='$GSBT_OPTS' sbt $GCMD_OPTS $PROXY_OPTS $argv"
 end
 
+# SBT using Omnia settings
+function osbt
+  set JVM_TMP_DIR "$HOME/workspace/tmp"
+  if not test -d "$JVM_TMP_DIR"
+    mkdir -p "$JVM_TMP_DIR"
+  end
+  set TR_REPO "$HOME/workspace/tooling.repositories"
+  if not test -d "$TR_REPO"
+    pushd "$HOME/workspace"
+    git clone https://github.source.internal.cba/AnalyticsInformation/tooling.repositories.git tooling.repositories
+    popd
+  end
+  set STANDARD_JVM_OPTS "-J-Djava.io.tmpdir=$HOME/workspace/tmp -J-Dfile.encoding=UTF8 -J-XX:MaxPermSize=2G -J-Xms512m -J-Xmx3g -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC"
+  set JVM_OPTS "-J-Dsbt.override.build.repos=true -J-Dsbt.repository.config=$TR_REPO/repositories $STANDARD_JVM_OPTS"
+  eval "env SBT_OPTS='' sbt $JVM_OPTS $argv"
+end
+
 # SBT using ZBI settings
 function zsbt
   set JVM_TMP_DIR "$HOME/workspace/tmp"
