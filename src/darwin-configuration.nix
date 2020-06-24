6487f69f3017e9dd68dd6cf098ac46b99f99127d
Jonathan Merritt <j.s.merritt@gmail.com>
2020-06-24 19:23:29 +1000

    Add texlive

--- src/darwin-configuration.nix
+++ src/darwin-configuration.nix
@@ -21,6 +21,7 @@
       pkgs.purescript
       pkgs.python3
       pkgs.spago  # Purescript build/package tool
+      pkgs.texlive.combined.scheme-full  # note: may need to fix zziplib bug: https://github.com/gdraheim/zziplib/pull/96
       pkgs.tree
       pkgs.vim
     ];
