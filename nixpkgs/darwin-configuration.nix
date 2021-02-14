f7c22b4dafeed00dcb8fd62c43e6f315dffc5c7a
Jonathan Merritt <j.s.merritt@gmail.com>
2021-02-14 20:42:45 +1100

    Start of user config

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -16,4 +16,34 @@
     enableFzfCompletion = true;
     enableFzfHistory = true;
   };
+
+  users.users.jsm = {
+    home = "/Users/jsm";
+    description = "Jonathan Merritt";
+  };
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.users.jsm = { pkgs, ... }: {
+    home.packages =
+      with pkgs;
+      [
+        alacritty
+        cabal-install
+        emacs
+        emacs-all-the-icons-fonts
+        fd
+        ghc
+        git
+        gnupg
+        iterm2
+        neovim
+        niv
+        ripgrep
+        texlive.combined.scheme-full
+        tmux
+        vim
+        zsh-powerlevel10k
+      ];
+  };
+
 }
