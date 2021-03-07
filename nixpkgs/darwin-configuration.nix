bfe987bf26b4cc853a746be61e99e6c1d53c14ef
Jonathan Merritt <jmerritt@gh.st>
2021-03-07 14:53:10 +1100

    Add powerlevel10k to zsh config

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -47,6 +47,18 @@
     programs.zsh = {
       enable = true;
       enableCompletion = true;
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource ./p10k-config;
+          file = "p10k.zsh";
+        }
+      ];
     };
   };
 
