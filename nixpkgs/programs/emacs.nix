ee24ed7406791f99a3802eb5c2fac35f3fe447d8
Jonathan Merritt <jmerritt@gh.st>
2021-06-16 17:44:24 +1000

    Switch to doom config again

--- nixpkgs/programs/emacs.nix
+++ nixpkgs/programs/emacs.nix
@@ -11,4 +11,20 @@
   #   ];
   # };
   home.file.".spacemacs".source = ./spacemacs.el;
+
+  home.file.".doom.d" = {
+    source = ./doom.d;
+    recursive = true;
+    onChange =
+      ''# Install doom emacs if it's not there already
+        if [! -d '~/.emacs.d']; then
+          git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+          ~/.emacs.d/bin/doom install
+        fi
+
+        # Perform a sync
+        echo "Doom sync"
+        ~/.emacs.d/bin/doom sync
+      '';
+  };
 }
