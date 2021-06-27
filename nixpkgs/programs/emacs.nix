8b0f8f04eebf33a2da213a10df06b66edea0d04f
Jonathan Merritt <jmerritt@gh.st>
2021-06-27 20:55:20 +1000

    Remove doom emacs sync

--- nixpkgs/programs/emacs.nix
+++ nixpkgs/programs/emacs.nix
@@ -15,16 +15,5 @@
   home.file.".doom.d" = {
     source = ./doom.d;
     recursive = true;
-    onChange =
-      ''# Install doom emacs if it's not there already
-        if [! -d '~/.emacs.d']; then
-          git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-          ~/.emacs.d/bin/doom install
-        fi
-
-        # Perform a sync
-        echo "Doom sync"
-        ~/.emacs.d/bin/doom sync
-      '';
   };
 }
