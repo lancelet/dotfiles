c3ea37b6f42adc6e62db595a2fb6fa2fd1739553
Jonathan Merritt <j.s.merritt@gmail.com>
2022-09-27 12:29:16 +1000

    Add ef-themes; use ef-deuteranopia-dark

--- nixpkgs/programs/doom.d/config.el
+++ nixpkgs/programs/doom.d/config.el
@@ -34,7 +34,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;;(setq doom-theme 'modus-vivendi)
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'ef-deuteranopia-dark)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
