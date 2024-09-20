34994c5337118dd2b5ffb4a1a26979aa3212e96d
Jonathan Merritt <j.s.merritt@gmail.com>
2024-09-21 01:18:45 +1000

    Add eza/exa

--- apps/zsh.nix
+++ apps/zsh.nix
@@ -2,5 +2,12 @@
 {
   programs.zsh = {
     enable = true;
+    initExtra = ''
+      alias ls='eza'
+      alias ll='eza -l'
+      alias lt='eza -lT'
+      export EDITOR='nvim'
+      export VISUAL='mvim'
+    '';
   };
 }
--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@
     packages = [
       pkgs.bat
       pkgs.dejavu_fonts
+      pkgs.eza
       pkgs.neovim
       pkgs.nerdfonts
       pkgs.tree
